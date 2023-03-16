de3ec3f809fc7f3f5f465916fbf2346130b675a5
Omolola Oladeinde <aderonke11@gmail.com>
2023-03-16 11:28:02 +0100

    add breakpoints

--- button.js
+++ button.js
@@ -34,3 +34,24 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+/*
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.navbar');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
+
+const navLinks = document.querySelectorAll('.navbar');
+
+navLinks.forEach(navLink => {
+  navLink.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+  });
+});
+
+
+*/
