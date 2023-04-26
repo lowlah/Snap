0714cf603551b2f83e68411b2430558985b99aa2
Omolola Oladeinde <aderonke11@gmail.com>
2023-04-26 13:35:07 +0100

    Update to Es6

--- button.js
+++ button.js
@@ -1,32 +1,11 @@
-/*const toggleButton= document.getElementsByClassName('toggle-button')[0]
-const navbarLinks= document.getElementsByClassName('links')[0]
-const header=document.getElementsByClassName('sale')
-
-toggleButton.addEventListener('click',() =>{ navbarLinks.classList.toggle('active');
-header.classList.toggle('sale')
-
-
-})*/
-
-const hamburger = document.getElementsByClassName('hamburger')[0];
+/*const hamburger = document.getElementsByClassName('hamburger')[0];
 const navMenu = document.getElementsByClassName('navbar')[0];
-
-
 
 hamburger.addEventListener('click', ()=>{
     hamburger.classList.toggle("active");
     navMenu.classList.toggle('active');
     document.querySelector("body").style.backgroundColor = "rgba(0,0,0,0.5)";
-
-    
-
 });
-
-
-/*function mobileMenu() {
-    hamburger.classList.toggle('active');
-    navMenu.classList.toggle('active');
-};*/
 
 const navLink = document.querySelectorAll(".navbar");
 
@@ -36,15 +15,15 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
     document.querySelector('body').style.backgroundColor = '';
-}
+}*/
 
-/*
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.navbar');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
+  document.body.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
 });
 
 const navLinks = document.querySelectorAll('.navbar');
@@ -53,8 +32,8 @@ navLinks.forEach(navLink => {
   navLink.addEventListener('click', () => {
     hamburger.classList.remove('active');
     navMenu.classList.remove('active');
+    /*document.body.style.backgroundColor = '';*/
+    document.querySelector("body").removeAttribute("style");
+
   });
 });
-
-
-*/
